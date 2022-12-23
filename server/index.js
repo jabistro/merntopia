aecd094041c45cafab314356a13219326f39a4f8
John Allan Hinds <jabistro@gmail.com>
2022-12-22 22:33:53 -0500

    backend setup

--- server/index.js
+++ server/index.js
@@ -14,6 +14,10 @@ import postRoutes from "./routes/posts.js";
 import { register } from "./controllers/auth.js";
 import { createPost } from "./controllers/posts.js";
 import { verifyToken } from "./middleware/auth.js";
+import User from "./models/User.js";
+import Port from "./models/Post.js";
+import { users, posts } from "./data/index.js";
+import Post from "./models/Post.js";
 
 /* CONFIGURATIONS */
 const __filename = fileURLToPath(import.meta.url);
@@ -57,5 +61,9 @@ mongoose
   })
   .then(() => {
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+
+    /* ADD DATA ONE TIME */
+    // User.insertMany(users);
+    // Post.insertMany(posts);
   })
   .catch((error) => console.log(`${error} did not connect.`));
