7fb667ca989a47147f1825fa58ce720a86b10c06
John Allan Hinds <jabistro@gmail.com>
2023-01-04 01:11:40 -0500

    Friend component built

---
+++ client/src/scenes/widgets/PostWidget.jsx
@@ -0,0 +1,9 @@
+import {
+  ChatBubbleOutlineOutlined,
+  FavoriteBorderOutlines,
+  FavoriteOutlined,
+  ShareOutlined,
+} from "@mui/icons-material";
+import { Box, Divider, IconButton, Typography, useTheme } from "@mui/material";
+import FlexBetween from "components/FlexBetween";
+import Friend from "components/Friend";
