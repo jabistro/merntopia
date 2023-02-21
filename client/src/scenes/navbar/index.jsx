ad4b98e8636a10745320e54bd9a46a4ac913cb6a
John Allan Hinds <jabistro@gmail.com>
2023-02-21 15:18:44 -0500

    navbar typo

--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -56,7 +56,7 @@ const Navbar = () => {
             },
           }}
         >
-          Sociopedia
+          Merntopia
         </Typography>
         {isNonMobileScreens && (
           <FlexBetween
