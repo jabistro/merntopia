ebba8a08c3be9ab59b472164a397866a95bc535a
John Allan Hinds <jabistro@gmail.com>
2023-02-22 18:53:16 -0500

    test

--- server/index.js
+++ server/index.js
@@ -54,8 +54,7 @@ app.use("/posts", postRoutes);
 
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 6001;
-mongoose
-  .connect(process.env.MONGO_URL, {
+mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
