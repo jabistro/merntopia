f8f8a17f6413a3887a80cd5d8a5e550d2c7539e6
John Allan Hinds <jabistro@gmail.com>
2023-03-15 23:20:39 -0400

    test

--- client/src/components/UserImage.jsx
+++ client/src/components/UserImage.jsx
@@ -8,7 +8,7 @@ const UserImage = ({ image, size = "60px" }) => {
         width={size}
         height={size}
         alt="user"
-        src={`http://localhost:3001/assets/${image}`}
+        src={`https://merntopia-backend.onrender.com/assets/${image}`}
       />
     </Box>
   );
